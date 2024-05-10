565ddd3e02d1831fda348b3b54f6e13c7a377a31
AiQiYang <yty0202@163.com>
2024-05-10 11:28:51 +1000

    modify the assignment2

--- assignment2/FooCorporation.java
+++ assignment2/FooCorporation.java
@@ -1,24 +1,23 @@
 public class FooCorporation {
     public static void calSalary(double pay, int hour) {
         // Match teo errors
-        if (pay < 8.0) {
+        if (pay < 8.0 || hour > 60) {
             System.out.println("error");
-        }
-        double salary;
-        if (hour > 60) {
-            System.out.println("error");
-        } else if (hour < 40) {
-            salary = pay * (double)hour;
-            System.out.println("Total pay: $" + salary);
         } else {
-            salary = pay * 40.0;
-            salary += pay * 1.5 * (hour - 40);
-            System.out.println("Total pay: $" + salary);
+            double salary = 0.0;
+            int overTime = 0;
+            if (hour > 40) {
+                overTime = hour - 40;
+                hour = 40;
+            }
+            salary = pay * hour + 1.5 * pay * overTime;
+            System.out.println("Pay this employee: $" + salary);
         }
     }
     public static void main(String[] args) {
         calSalary(7.5, 35);
         calSalary(8.2, 47);
-        calSalary(10, 73);
+        calSalary(10, 40);
+        calSalary(12, 63);
     }
 }
